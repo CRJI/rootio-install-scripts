fc2a4b7dfb2373ef0b56e35884d216840a37786a
Alex Morega <alex@grep.ro>
2018-04-16 17:24:13 +0000

    change filename

--- install-ansible.sh
+++ install-ansible.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -ex
 
-echo 'deb http://ppa.launchpad.net/ansible/ansible/ubuntu trusty main' > /etc/apt/sources.list.d/ansible-trusty.list
+echo 'deb http://ppa.launchpad.net/ansible/ansible/ubuntu trusty main' > /etc/apt/sources.list.d/ansible.list
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367
 apt-get update -yqq
 apt-get install -yqq ansible
