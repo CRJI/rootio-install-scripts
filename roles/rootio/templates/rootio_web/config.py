19d8eb0c23b6c90677771d93eddf3b67cabd2326
Alex Morega <alex@grep.ro>
2018-04-18 11:51:21 +0000

    make station-telephony work

--- roles/rootio/templates/rootio_web/config.py
+++ roles/rootio/templates/rootio_web/config.py
@@ -13,6 +13,9 @@ class BaseConfig(object):
     ZMQ_BIND_ADDR = "tcp://127.0.0.1:55777"
     ZMQ_SOCKET_TYPE = "PUB"
     ACCEPT_LANGUAGES = {'en':'English'}
+    SCHEDULE_EVENTS_SERVER_IP = '127.0.0.1'
+    SCHEDULE_EVENTS_SERVER_PORT = 12345
+    SQLALCHEMY_DATABASE_URI = 'postgres:///rootio'
 
 class DefaultConfig(BaseConfig):
     pass
